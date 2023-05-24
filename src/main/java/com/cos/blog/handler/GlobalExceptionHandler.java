b7197ff4442ab9d77a4ee132b0361020a423e546
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-05-24 23:27:36 +0900

    2023-05-24 로그인 작업
    - 이론위주로 공부했음.
    - 스프링부트에서 트랜잭션이 어떻게 진행되는 지, OSIV기법도 공부하였음.

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.cos.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.dto.ResponseDto;
 
 @ControllerAdvice // 모든 예외가 발생하면 이 클래스로 들어오게 하는 어노테이션.
 @RestController
 public class GlobalExceptionHandler { // 예외처리 하는 클래스
 	
 	@ExceptionHandler(value=Exception.class) // IllegalArgumentException 발생하면 이 함수에 전달하여 getMessage()가 리턴된다.
-	public String handleArgumentException(Exception e) {
-		return "<h1>" + e.getMessage() + "<h1>";
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
 	}
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -33,7 +33,36 @@ let index = {
 			alert(JSON.stringify(error));
 			
 		}); // ajax 통신을 이용해서 3개의 파라미터를 json으로 변경하여 insert 요청 
+    },
+    
+     login:function(){
+        let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+		};
+		
+		//console.log(data)
+		//ajax 통신을 이용하여 3개의 데이터를 json으로 변경하여 insert
+		//ajax가 통신 성공하고, 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환.
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user/login",
+			//stringify : 자바스크립트 오브젝트를 JSON으로 변경.
+			data: JSON.stringify(data), //HTTP Body 데이터 -> MIME 타입 설정이 필요함.
+			//contentType: body 데이터가 어떤 타입인지 설정.
+			contentType: "application/json; charset=utf-8",
+			//dataType : 응답 데이터를 어떻게 받을 지 설정.
+			dataType: "json" // 응답 
+		}).done(function(resp){
+			alert("로그인이 완료되었습니다.");
+//			console.log(resp)
+			location.href = "/blog";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+			
+		}); // ajax 통신을 이용해서 3개의 파라미터를 json으로 변경하여 insert 요청 
     }
+    
 }
 
 index.init();
